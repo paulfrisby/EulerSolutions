1428fdcdfdaed93d7df17f80a0c5230c88bdd3e4
Paul Frisby <mail@paulfrisby.com>
2020-10-25 21:09:55 +0000

    Problem 4 refactored, commented, and updated to python 3 / to fit template

--- Solutions/Project Euler - 004 - Largest palindrome product.py
+++ Solutions/Project Euler - 004 - Largest palindrome product.py
@@ -1,33 +1,61 @@
-def reverse(n):
-    revn = 0
-    while n > 0:
-        revn = revn*10 + n%10
-        n = int (n / 10)
-    return revn
+# ------------------------------------------------------------------------------
+# Project Euler - Problem 004 - Template File
+# ------------------------------------------------------------------------------
+# Problem Link: https://projecteuler.net/problem=004
+# ------------------------------------------------------------------------------
+# Author: Paul Frisby
+# Email: mail@paulfrisby.com
+# Github: https://github.com/paulfrisby/
+# ------------------------------------------------------------------------------
 
-def palindromic(n):
-    return n == reverse(n)
+
+# ------------------------------------------------------------------------------
+# Problem Definition
+# ------------------------------------------------------------------------------
+"""
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Main Code
+# ------------------------------------------------------------------------------
+
+def isPalindromic(n):
+    return str(n) == str(n)[::-1]
 
 def largestPalindromicProduct(digits, printcalc=""):
+    
+    # initialising x and y to largest possible 3 digit numbers
     x = 10**digits - 1
     y = 10**digits - 1
-    pal = 0
-    palx = 0
-    paly = 0
-    while y > 0 and y*(10**digits-1) > pal:
-        while x > 0 and x*y > pal:
-            if palindromic(x*y) and x*y > pal:
-                pal = x*y
-                palx = x
-                paly = y
-            x = x - 1
-        y = y - 1
+
+    # initialising variables to store largest palindromic product, and the factors used to produce it
+    product = 0
+    factx = 0
+    facty = 0
+
+    # checks all combination of x & y,
+    # loop ends early if no possible multiple of factors could be larger than largest product already found.
+    # iterates backwards towards 0 as we are looking for largest product,
+    # ignoring smaller products without having to compute every single one is more efficient
+    while y > 0 and y*(10**digits-1) > product:
+        while x > 0 and x*y > product:
+            if isPalindromic(x*y):
+                product = x*y
+                factx = x
+                facty = y
+            x -= 1
+        y -= 1
         x = 10**digits - 1
+
+    # prints out full calculation for result if optional argument included    
     if printcalc == "y":
-        print (palx, "*", paly, "=", pal)
-    return pal
+        print (factx, "*", facty, "=", product)
+        
+    return product
+
 
 largestPalindromicProduct(3, printcalc="y")
-
-
-
