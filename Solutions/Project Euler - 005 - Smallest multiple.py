fee316c367f32fbcc1105bd245a083ef9107048d
Paul Frisby <mail@paulfrisby.com>
2020-10-25 21:58:17 +0000

    Problem 5 refactored, commented, checked working with python 3, and updated to fit template

--- Solutions/Project Euler - 005 - Smallest multiple.py
+++ Solutions/Project Euler - 005 - Smallest multiple.py
@@ -1,13 +1,55 @@
-def smallestDiv(upto):
-    x = 1
-    prevx = 0
-    i = 1
-    while i < upto + 1:
-        if x%i==0:
-            prevx = x
-            i = i + 1
-        else:
-            x = x + prevx
-    return x
+# ------------------------------------------------------------------------------
+# Project Euler - Problem 005 - Template File
+# ------------------------------------------------------------------------------
+# Problem Link: https://projecteuler.net/problem=005
+# ------------------------------------------------------------------------------
+# Author: Paul Frisby
+# Email: mail@paulfrisby.com
+# Github: https://github.com/paulfrisby/
+# ------------------------------------------------------------------------------
 
-print smallestDiv(20)
+
+# ------------------------------------------------------------------------------
+# Problem Definition
+# ------------------------------------------------------------------------------
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Pseudocode
+# ------------------------------------------------------------------------------
+"""
+general outline / plan of approach to problem
+explanation of insights in to problem may also be included here as necessary
+"""
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Main Code
+# ------------------------------------------------------------------------------
+
+def smallestMultiple(largestFactor):
+
+    # setup for first loop runthrough
+    smallestMultiple = 1
+    previousMultiple = 1
+    
+    # iterates through every factor
+    for i in range(1, largestFactor + 1):
+
+        # runs until smallestMultiple is evenly divisible with current factor
+        while not smallestMultiple%i==0:
+            # adds smallestMultiple from previously calculated factors so new number is still divisible by all of them
+            smallestMultiple += previousMultiple
+
+        # setup for next loop runthrough
+        previousMultiple = smallestMultiple
+    
+    return smallestMultiple
+
+print (smallestMultiple(20))
