d64ad073e6eb54db4e06c89f5439c69ed0cadc27
Paul Frisby <mail@paulfrisby.com>
2020-10-26 16:16:25 +0000

    problem 6 refactored, commented, updated to python 3, updated to fit template

--- Solutions/Project Euler - 006 - Sum square difference.py
+++ Solutions/Project Euler - 006 - Sum square difference.py
@@ -1,21 +1,52 @@
-def sumOfPowers(power, upto):
-    x = 1
-    sumx = 0
-    while x - 1 < upto:
-        sumx = sumx + x**power
-        x = x + 1
-    return sumx
+# ------------------------------------------------------------------------------
+# Project Euler - Problem 006 - Sum square difference
+# ------------------------------------------------------------------------------
+# Problem Link: https://projecteuler.net/problem=006
+# ------------------------------------------------------------------------------
+# Author: Paul Frisby
+# Email: mail@paulfrisby.com
+# Github: https://github.com/paulfrisby/
+# ------------------------------------------------------------------------------
 
-def powerOfSums(power, upto):
-    x = 1
-    sumx = 0
-    while x - 1 < upto:
-        sumx = sumx + x
-        x = x + 1
-    return sumx**power
 
-def diffOfPowers(power, upto):
-    return powerOfSums(power, upto) - sumOfPowers(power, upto)
+# ------------------------------------------------------------------------------
+# Problem Definition
+# ------------------------------------------------------------------------------
+"""
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2+...+10^2 = 385
 
-print diffOfPowers(2, 100)
+The square of the sum of the first ten natural numbers is,
+(1+2+...+10)^2 = 55^2 = 3025
 
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Main Code
+# ------------------------------------------------------------------------------
+
+# when power = n, returns sum of nth powers up to limit
+def sumOfPowers(power, limit):
+    runningSum = 0
+    for i in range(1, limit + 1):
+        runningSum += i**power
+    return runningSum
+
+
+# when power = n, returns nth power of sum of n up to limit
+def powerOfSums(power, limit):
+    runningSum = 0
+    for i in range(1, limit + 1):
+        runningSum += i
+    return runningSum**power
+
+def diffOfPowers(power, limit):
+    return powerOfSums(power, limit) - sumOfPowers(power, limit)
+
+print (diffOfPowers(2, 100))
