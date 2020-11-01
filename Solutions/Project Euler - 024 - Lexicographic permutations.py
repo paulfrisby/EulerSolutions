77fa26b9e5a0c73d3c9bad9c95756b56910fdab2
Paul Frisby <mail@paulfrisby.com>
2020-11-01 06:38:32 +0000

    problem 24 commented, variable names updated, and pseudocode added

--- Solutions/Project Euler - 024 - Lexicographic permutations.py
+++ Solutions/Project Euler - 024 - Lexicographic permutations.py
@@ -31,17 +31,49 @@ What is the millionth lexicographic permutation of the digits
 # Pseudocode
 # ------------------------------------------------------------------------------
 """
-general outline / plan of approach to problem
-explanation of insights in to problem may also be included here as necessary
-"""
-# ------------------------------------------------------------------------------
+to find nth permutation of x objects
+
+n = n-1
+
+for each position in permutation:
+    binsize = x! / x
+    i = int(n / binsize)
+    let the ith ordered object be in this position
+    delete ith object from objects
+
+    x = x - 1
+    n = n % binsize
 
 
-# ------------------------------------------------------------------------------
-# Extra Information
-# ------------------------------------------------------------------------------
-"""
-optional section depending on problem
+For example, in the case of finding 4th permutation of 0, 1 & 2:
+
+(n = 4, x = 3, digits = [0,1,2])
+
+n = n - 1 = 4 - 1 = 3
+
+position 1:
+    binsize = x! / x = 3! / 3 = 6 / 3 = 2
+    i = int(n/binsize) = int(3/2) = 1
+    position 1 is digits[1] = 1
+
+    remaining digits = [0,2]
+    x = x - 1 = 3 - 1 = 2
+    n = n % binsize = 3 % 2 = 1
+
+position 2:
+    binsize = x! / x = 2! / 2 = 2 / 2 = 1
+    i = int(n/binsize) = int(1/1) = 1
+    position 2 is digits[1] = 2
+
+    remaining digits = [0]
+    x = x - 1 = 2 - 1 = 1
+    n = n % binsize = 1 % 1 = 0
+
+position 3:
+    since binsize is now 0, we only have 1 option left
+    position 3 is 0
+
+The 4th permutation of 0, 1 & 2 is '120'
 """
 # ------------------------------------------------------------------------------
 
@@ -50,25 +82,47 @@ optional section depending on problem
 # Main Code
 # ------------------------------------------------------------------------------
 
+# returns n!
 def factorial(n):
-    f = n
-    for i in range(1, n):
-        f = f * i
-    return f
+    factorial = 1
+    for i in range(1, n+1):
+        factorial = factorial * i
+    return factorial
+
 
 # returns nth permutation (ordered lexicographically) of elements in objects
 def perm(objects, n):
-    objects.sort()
-    p = ''
-    n -= 1
-    x = len(objects)
-    while x > 1:
-        tot = factorial(x) / x 
-        p += str(objects[int(n/tot)])
-        del objects[int(n/tot)]
-        n = n % tot
-        x -= 1
-    p += str(objects[0])
-    return p
 
-print (perm([0, 9, 8, 7, 6, 5, 4, 3, 2, 1], 1000000))
+    length = len(objects)
+
+    # error catching
+    # there are exactly x! possible permutations, where objects has x elements
+    if factorial(length) < n:
+        return f'There are not {n} possible permutations of the objects in {objects}'
+    
+    # in case objects are not passed already in lexicographic order
+    objects.sort()
+    permutation = ''
+    n -= 1
+    
+    while length > 1:
+
+        # amount of permutations with each object in next position
+        binsize = factorial(length) / length
+        
+        # find out which object is next by finding out which bin n falls in
+        permutation += str(objects[int(n/binsize)])
+
+        # this object can not be reused, so delete it
+        del objects[int(n/binsize)]
+
+        # update values for next calculation
+        n = n % binsize
+        length -= 1
+
+    # add last remaining object to permutation    
+    permutation += str(objects[0])
+    return permutation
+
+
+print (f'The millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 & 9 is {perm([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1000000)}')
