8d0ba70cf02d845e1e417b0846138987368a9953
Paul Frisby <mail@paulfrisby.com>
2020-11-02 11:55:11 +0000

    problem 30 refactored and commented

--- Solutions/Project Euler - 030 - Digit fifth powers.py
+++ Solutions/Project Euler - 030 - Digit fifth powers.py
@@ -34,17 +34,13 @@ of their digits.
 # Pseudocode
 # ------------------------------------------------------------------------------
 """
-general outline / plan of approach to problem
-explanation of insights in to problem may also be included here as necessary
-"""
-# ------------------------------------------------------------------------------
+9^5 - 59,049 is the highest 5th power that can contribute to our sum
 
+6 digit numbers have max sum of 5th powers of digits of 6 * 59,043 = 354,294
+7 digit numbers have max sum of 5th powers of digits of 7 * 59,043 = 413,343
 
-# ------------------------------------------------------------------------------
-# Extra Information
-# ------------------------------------------------------------------------------
-"""
-optional section depending on problem
+All 7 digit numbers are large than this, and the situation will only get worse
+as we add more digits. Therefore, we only need to check up to 6 digit numbners.
 """
 # ------------------------------------------------------------------------------
 
@@ -53,18 +49,24 @@ optional section depending on problem
 # Main Code
 # ------------------------------------------------------------------------------
 
-def isfifthSum(n):
-    num = n
-    fifthsum = 0
-    while num > 0:
-        fifthsum += (num % 10)**5
-        num = int (num/10)
-    return fifthsum == n
+# returns sum of 5th powers of digits of n
+def fifthSum(n):
+    fifthSum = 0
+    while n > 0:
+
+        # add last digit ^ 5 to sum
+        fifthSum += (n%10)**5
+
+        # truncate last digit
+        n = int(n/10)
+        
+    return fifthSum
 
 
-fifth = []
-for i in range(2, 300000):
-    if isfifthSum(i):
-        fifth += [i]
+answers = []
+for i in range(10, 300000):
+    if fifthSum(i) == i:
+        answers += [i]
 
-print (fifth, sum(fifth))
+print (f'The numbers that can be written as the sum of fifth powers of their digits are: {answers}')
+print (f'The sum of these is {sum(answers)}')
